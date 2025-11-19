102ed282de151d279a69bbf554fac9bf67a342d8
vuthevy1209 <vuthevy12092004@gmail.com>
2025-11-19 18:42:22 +0700

    init project

---
+++ src/main/java/com/finalproject/example/EmailClientAI/EmailClientAiApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.example.EmailClientAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailClientAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailClientAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/example/EmailClientAI/EmailClientAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.example.EmailClientAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClientAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
